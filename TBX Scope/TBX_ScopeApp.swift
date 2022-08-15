4438eb167ddc7a19d6cfa05cff2603c0a367108b
David Bureš <buresdv@gmail.com>
2022-08-15 18:46:44 +0200

    Initial Commit

---
+++ TBX Scope/TBX_ScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TBX_ScopeApp.swift
+//  TBX Scope
+//
+//  Created by David Bureš on 15.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TBX_ScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
